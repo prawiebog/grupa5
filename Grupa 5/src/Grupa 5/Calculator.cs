9957b5a88a2187a335ad7e9bca23faf7b53f90fd
prawiebog <prawie_bog@wp.pl>
2016-11-29 10:44:51 +0100

    dodany kjhkhjś

--- Grupa 5/src/Grupa 5/Calculator.cs
+++ Grupa 5/src/Grupa 5/Calculator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-/* jakiś fajny komentarz */
+/* jakiś fajny komentarz kjhkhjś*/
 namespace Grupa_5
 {
     public class Calculator
